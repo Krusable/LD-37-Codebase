26c780b329da2d46fa2efa937e5b7a4699a2c84c
Mathew Bergen <Mathew_bergen@yahoo.ca>
2016-12-09 16:15:26 -0800

    font rendering

--- src/ld_37.cpp
+++ src/ld_37.cpp
@@ -1,220 +1,14 @@
 
 /*
     TODO For LD:
-    - Load pngs
-    - Render textures
-    - Render sub textures
-    - load map files
     - load audio files
     - play sound effects & music 
 */
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include <stdint.h>
-
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-typedef float f32;
-typedef double f64;
-
-typedef struct {
-    i32 width;
-    i32 height;
-    i64 pixel_buffer_size;
-    i32 pixel_buffer_pitch;
-    i32 pixels_per_meter;
-    SDL_PixelFormat* pixel_format;
-    void* pixel_buffer;
-    char* title;
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    SDL_Texture* texture;
-} Display;
-
-typedef struct {
-    i32 width;
-    i32 height;
-    void* pixels;
-} Texture;
-
-typedef union {
-    struct {
-        f32 x;
-        f32 y;
-    };
-
-    struct {
-        f32 w;
-        f32 h;
-    };
-} Vec2;
-
-typedef union {
-    struct {
-        f32 x;
-        f32 y;
-        f32 z;
-        f32 w;
-    };
-
-    struct {
-        f32 a;
-        f32 r;
-        f32 g;
-        f32 b;
-    };
-} Vec4;
+#include "ld_37_lib.h"
 
 #define TARGET_MS_PER_FRAME 16
 #define CAP_FRAMERATE       1
-#define TRANSPARENT_COLOUR  0x00FF00FF
-
-u32 ColourVec4ToU32(Vec4* vec4_colour) {
-    u8 a = vec4_colour->a * 255;
-    u8 r = vec4_colour->r * 255;
-    u8 g = vec4_colour->g * 255;
-    u8 b = vec4_colour->b * 255;
-
-    u32 u32_colour = (a << 24) | (r << 16) | (g << 8) | b;
-    return u32_colour;
-}
-
-void RenderFilledRect(Display* display, Vec2* pos, Vec2* size, u32 colour) {
-    i32 y_min           = pos->y * display->pixels_per_meter;
-    i32 y_max           = (pos->y + size->h) * display->pixels_per_meter;
-    i32 render_width    = size->w * display->pixels_per_meter;
-
-    for(i32 y = y_min; y < y_max; y++) {
-        if(y < 0) {
-            continue;
-        }
-        // if the rect is off the screen completly don't render
-        else if(y >= display->height) {
-            return;
-        }
-
-        i32 x_min = pos->x * display->pixels_per_meter;
-        i32 x_max = (pos->x + size->w) * display->pixels_per_meter;
-
-        if(x_min < 0) {
-            // if the rect is off the screen completly don't render
-            if(x_min <= -render_width) {
-                return;
-            }
-
-            // NOTE (Mathew): x is negative here so the result will be a smaller width; the width
-            //                of the remaining rectangle we want to render
-            x_max = x_min + render_width;
-            render_width = x_max - x_min;
-            x_min = 0;
-        }
-
-        u32* pixel = (u32*)display->pixel_buffer + (y * display->width) + x_min;
-
-        for(i32 x = x_min; x < x_max; x++) {
-            if(y < display->height  && y >= 0 && x < display->width && x >= 0) {
-                *pixel++ = colour;
-            }
-        }
-    }
-}
-
-void RenderFilledRect(Display* display, Vec2* pos, Vec2* size, Vec4* vec4_colour) {
-    RenderFilledRect(display, pos, size, ColourVec4ToU32(vec4_colour));
-}
-
-void RenderTexture(Display* display, Vec2* pos, Texture* texture) {
-    i32 x_min = pos->x * display->pixels_per_meter;
-    i32 x_max = x_min + texture->width;
-    i32 y_min = pos->y * display->pixels_per_meter;
-    i32 y_max = y_min + texture->width;
-
-    // Don't bother rendering if the texture is completly off the screen.
-    if(x_max < 0 || x_min >= display->width || x_max < 0 || x_min >= display->width) {
-        return;
-    }
-
-    for(i32 y = y_min, ty = 0; y < y_max; y++, ty++) {
-        for(i32 x = x_min, tx = 0; x < x_max; x++, tx++) {
-            if(x >= 0 && x < display->width && y >= 0 && y < display->height) {
-                u32* display_pixel = (u32*)display->pixel_buffer + (y * display->width) + x;
-                u32* texture_pixel = (u32*)texture->pixels + (ty * texture->width) + tx;
-                if(*texture_pixel != TRANSPARENT_COLOUR) {
-                    *display_pixel = *texture_pixel;
-                }
-            }
-        }
-    }
-}
-
-void RenderSubTexture(Display* display, Vec2* pos, Texture* texture, i32 sub_texture_x, i32 sub_texture_y, i32 sub_texture_w, i32 sub_texture_h) {
-    i32 x_min = pos->x * display->pixels_per_meter;
-    i32 x_max = x_min + sub_texture_w;
-    i32 y_min = pos->y * display->pixels_per_meter;
-    i32 y_max = y_min + sub_texture_h;
-
-    // Don't bother rendering if the texture is completly off the screen.
-    if(x_max < 0 || x_min >= display->width || x_max < 0 || x_min >= display->width) {
-        return;
-    }
-
-    for(i32 y = y_min, ty = sub_texture_y; y < y_max; y++, ty++) {
-        for(i32 x = x_min, tx = sub_texture_x; x < x_max; x++, tx++) {
-            if(x >= 0 && x < display->width && y >= 0 && y < display->height) {
-                u32* display_pixel = (u32*)display->pixel_buffer + (y * display->width) + x;
-                u32* texture_pixel = (u32*)texture->pixels + (ty * texture->width) + tx;
-                if(*texture_pixel != TRANSPARENT_COLOUR) {
-                    *display_pixel = *texture_pixel;
-                }
-            }
-        }
-    }
-}
-
-void LoadMeSomePNG(char* file_path, Texture* dst_texture, SDL_PixelFormat* pixel_format) {
-    SDL_assert(dst_texture->pixels);
-
-    SDL_Surface* loaded_texture = NULL;
-
-    loaded_texture = IMG_Load(file_path);
-    if(!loaded_texture) {
-        printf("Warning - could not load image '%s'.\n", file_path);
-        return;
-    }
-
-    SDL_Surface* corrected_texture = SDL_ConvertSurface(loaded_texture, pixel_format, 0);
-    if(!corrected_texture) {
-        printf("Warning - could not convert image. %s.\n", SDL_GetError());
-        return;
-    }
-
-    dst_texture->width = corrected_texture->w;
-    dst_texture->height = corrected_texture->h;
-    i32 tex_size = dst_texture->width * dst_texture->height;
-    dst_texture->pixels = malloc(tex_size * pixel_format->BytesPerPixel);
-    for(i32 i = 0; i < tex_size; i++) {
-        ((u32*)dst_texture->pixels)[i] = ((u32*)corrected_texture->pixels)[i];
-    }
-
-    SDL_FreeSurface(loaded_texture);
-    SDL_FreeSurface(corrected_texture);
-}
-
-void FreeTexture(Texture* texture) {
-    free(texture->pixels);
-    texture->pixels = 0;
-}
 
 i32 main(i32 argc, char** argv) {
     srand(time(0));
@@ -227,7 +21,7 @@ i32 main(i32 argc, char** argv) {
     if(IMG_Init(flags) & flags != flags) {
         printf("Error - Could not init SDL_Image. IMG_Error: %s\n", IMG_GetError());
         return -1;
-    } \
+    }
 
     Display display = {0};
     display.width = 1024;
@@ -267,7 +61,7 @@ i32 main(i32 argc, char** argv) {
     SDL_Event event = {0};
     Vec2 player_pos = {300 / display.pixels_per_meter, 300 / display.pixels_per_meter};
     Vec2 player_size = {100 / display.pixels_per_meter, 100 / display.pixels_per_meter};
-    Vec4 player_colour = {1.0f, 0.2f, 0.2f, 0.2f};
+    u32 player_colour = 0xFF111111;
     f32 speed = 2 * ((f32)display.pixels_per_meter / (1000.0 / (f32)TARGET_MS_PER_FRAME));
     f32 last_delta = 0.0f;
 
@@ -278,11 +72,12 @@ i32 main(i32 argc, char** argv) {
 
     Vec2 font_rect_pos = {10.0 / (f32)display.pixels_per_meter, 10.0 / (f32)display.pixels_per_meter};
     Vec2 font_rect_size = {10, 4};
-    Vec4 font_rect_colour = {1.0f, 0.8f, 0.8f, 0.8f};
+    u32 font_rect_colour = 0xFFAAAAAA;
 
-    Vec2 texture_pos = {1, 1};
-    Texture font_texture = {0};
-    LoadMeSomePNG("../res/textures/font_texture.png", &font_texture, display.pixel_format);
+    Vec2 test_string_pos = {1.1, 1.1};
+
+    Font test_font = {0};
+    LoadFont(&test_font, display.pixel_format);
 
     for(i32 y = 0; y < MAP_HEIGHT; y++) {
         for(i32 x = 0; x < MAP_WIDTH; x++) {
@@ -332,10 +127,9 @@ i32 main(i32 argc, char** argv) {
             }
         }
 
-        RenderFilledRect(&display, &font_rect_pos, &font_rect_size, &font_rect_colour);
-        RenderFilledRect(&display, &player_pos, &player_size, &player_colour);
-        //RenderTexture(&display, &texture_pos, &font_texture);
-        RenderSubTexture(&display, &texture_pos, &font_texture, 32, 32, 32, 32);
+        RenderFilledRect(&display, &font_rect_pos, &font_rect_size, font_rect_colour);
+        RenderString(&display, &test_font, &test_string_pos, "TEST 123 ABC", 0xFFE7E7E7);
+        RenderFilledRect(&display, &player_pos, &player_size, player_colour);
 
         SDL_UpdateTexture(display.texture, 0, display.pixel_buffer, display.pixel_buffer_pitch);
         SDL_RenderCopy(display.renderer, display.texture, NULL, NULL);
@@ -354,7 +148,7 @@ i32 main(i32 argc, char** argv) {
         last_delta = ms_this_frame / 1000.0f;
     }
 
-    FreeTexture(&font_texture);
+    FreeFont(&test_font);
     free(display.pixel_buffer);
     SDL_DestroyTexture(display.texture);
     SDL_DestroyRenderer(display.renderer);
---
+++ src/ld_37_lib.h
@@ -0,0 +1,349 @@
+
+/*
+    Copyright (c) Mathew Bergen 2016
+
+    A library used for Ludum Dare 37. Written in SDL and 
+    C++. Anyone can use this code however they wish.
+ */
+
+#ifndef LD_37_LIB_H
+#define LD_37_LIB_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef float f32;
+typedef double f64;
+
+typedef struct {
+    i32 width;
+    i32 height;
+    i64 pixel_buffer_size;
+    i32 pixel_buffer_pitch;
+    i32 pixels_per_meter;
+    SDL_PixelFormat* pixel_format;
+    void* pixel_buffer;
+    char* title;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Texture* texture;
+} Display;
+
+typedef struct {
+    i32 width;
+    i32 height;
+    void* pixels;
+} Texture;
+
+typedef struct {
+    char* char_set;
+    i32 char_set_size;
+    Texture texture_sheet;
+    i32 char_width;
+    i32 char_height;
+    i32 tailed_char_offset;
+    i32 char_spacing;
+} Font;
+
+typedef union {
+    struct {
+        f32 x;
+        f32 y;
+    };
+
+    struct {
+        f32 w;
+        f32 h;
+    };
+} Vec2;
+
+typedef union {
+    struct {
+        f32 x;
+        f32 y;
+        f32 z;
+        f32 w;
+    };
+
+    struct {
+        f32 a;
+        f32 r;
+        f32 g;
+        f32 b;
+    };
+} Vec4;
+
+typedef struct {
+    u64 size;
+    void* data;
+} File_Read_Result;
+
+#define TRANSPARENT_COLOUR      0x00FF00FF
+#define REPLACEMENT_COLOUR_1    0x00000000
+
+u32 ColourVec4ToU32(Vec4* vec4_colour) {
+    u8 a = vec4_colour->a * 255;
+    u8 r = vec4_colour->r * 255;
+    u8 g = vec4_colour->g * 255;
+    u8 b = vec4_colour->b * 255;
+
+    u32 u32_colour = (a << 24) | (r << 16) | (g << 8) | b;
+    return u32_colour;
+}
+
+void RenderFilledRect(Display* display, Vec2* pos, Vec2* size, u32 colour) {
+    i32 x_min = pos->x * display->pixels_per_meter;
+    i32 x_max = x_min + (size->w * display->pixels_per_meter);
+    i32 y_min = pos->y * display->pixels_per_meter;
+    i32 y_max = y_min + (size->h * display->pixels_per_meter);
+
+    // Don't bother rendering if the rect is completly off the screen.
+    if(x_max < 0 || x_min >= display->width || y_max < 0 || y_min >= display->height) {
+        return;
+    }
+
+    for(i32 y = y_min; y < y_max; y++) {
+        for(i32 x = x_min; x < x_max; x++) {
+            if(x >= 0 && x < display->width && y >= 0 && y < display->height) {
+                u32* display_pixel = (u32*)display->pixel_buffer + (y * display->width) + x;
+                *display_pixel = colour;
+            }
+        }
+    }
+}
+
+void RenderTexture(Display* display, Vec2* pos, Texture* texture) {
+    i32 x_min = pos->x * display->pixels_per_meter;
+    i32 x_max = x_min + texture->width;
+    i32 y_min = pos->y * display->pixels_per_meter;
+    i32 y_max = y_min + texture->height;
+
+    // Don't bother rendering if the texture is completly off the screen.
+    if(x_max < 0 || x_min >= display->width || y_max < 0 || y_min >= display->height) {
+        return;
+    }
+
+    for(i32 y = y_min, ty = 0; y < y_max; y++, ty++) {
+        for(i32 x = x_min, tx = 0; x < x_max; x++, tx++) {
+            if(x >= 0 && x < display->width && y >= 0 && y < display->height) {
+                u32* display_pixel = (u32*)display->pixel_buffer + (y * display->width) + x;
+                u32* texture_pixel = (u32*)texture->pixels + (ty * texture->width) + tx;
+                if(*texture_pixel != TRANSPARENT_COLOUR) {
+                    *display_pixel = *texture_pixel;
+                }
+            }
+        }
+    }
+}
+
+void RenderSubTexture(Display* display, Vec2* pos, Texture* texture, i32 sub_texture_x, i32 
+                      sub_texture_y, i32 sub_texture_w, i32 sub_texture_h) {
+    i32 x_min = pos->x * display->pixels_per_meter;
+    i32 x_max = x_min + sub_texture_w;
+    i32 y_min = pos->y * display->pixels_per_meter;
+    i32 y_max = y_min + sub_texture_h;
+
+    // Don't bother rendering if the texture is completly off the screen.
+    if(x_max < 0 || x_min >= display->width || x_max < 0 || x_min >= display->width) {
+        return;
+    }
+
+    for(i32 y = y_min, ty = sub_texture_y; y < y_max; y++, ty++) {
+        for(i32 x = x_min, tx = sub_texture_x; x < x_max; x++, tx++) {
+            if(x >= 0 && x < display->width && y >= 0 && y < display->height) {
+                u32* display_pixel = (u32*)display->pixel_buffer + (y * display->width) + x;
+                u32* texture_pixel = (u32*)texture->pixels + (ty * texture->width) + tx;
+                if(*texture_pixel != TRANSPARENT_COLOUR) {
+                    *display_pixel = *texture_pixel;
+                }
+            }
+        }
+    }
+}
+
+// NOTE (Mathew): replaces black with the replacement colour
+void RenderSubTextureEx(Display* display, Vec2* pos, Texture* texture, i32 sub_texture_x, i32 sub_texture_y, 
+                        i32 sub_texture_w, i32 sub_texture_h, u32 replacement_colour1) {
+    i32 x_min = pos->x * display->pixels_per_meter;
+    i32 x_max = x_min + sub_texture_w;
+    i32 y_min = pos->y * display->pixels_per_meter;
+    i32 y_max = y_min + sub_texture_h;
+
+    // Don't bother rendering if the texture is completly off the screen.
+    if(x_max < 0 || x_min >= display->width || x_max < 0 || x_min >= display->width) {
+        return;
+    }
+
+    for(i32 y = y_min, ty = sub_texture_y; y < y_max; y++, ty++) {
+        for(i32 x = x_min, tx = sub_texture_x; x < x_max; x++, tx++) {
+            if(x >= 0 && x < display->width && y >= 0 && y < display->height) {
+                u32* display_pixel = (u32*)display->pixel_buffer + (y * display->width) + x;
+                u32* texture_pixel = (u32*)texture->pixels + (ty * texture->width) + tx;
+                if(*texture_pixel != TRANSPARENT_COLOUR) {
+                    if(*texture_pixel == REPLACEMENT_COLOUR_1) {
+                        *display_pixel = replacement_colour1;
+                    }
+                    else {
+                        *display_pixel = *texture_pixel;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void RenderString(Display* display, Font* font, Vec2* pos, char* string, u32 colour) {
+    i32 char_num = 0;
+    i32 x_min = pos->x * display->pixels_per_meter;
+    i32 y_min = pos->y * display->pixels_per_meter;
+
+    SDL_assert(font->texture_sheet.width % font->char_width == 0 && font->texture_sheet.height % font->char_height == 0);
+
+    i32 characters_wide = font->texture_sheet.width / font->char_width;
+    i32 characters_high = font->texture_sheet.height / font->char_height;
+    for(char* s = string; *s != '\0'; s++) {
+        Vec2 render_pos = {x_min + (char_num * (font->char_width + font->char_spacing)), y_min};
+        bool found = false;
+        i32 i = 0;
+        for(; i < font->char_set_size; i++) {
+            if(*s == font->char_set[i]) {
+                found = true;
+                break;
+            }
+        }
+
+        if(found) {
+            if(*s == 'p' || *s == 'y', *s == 'q', *s == 'j') {
+                render_pos.y += font->tailed_char_offset;
+            }
+
+            i32 sub_tex_y = (i / characters_high) * font->char_height;
+            i32 sub_tex_x = (i % characters_wide) * font->char_width;
+            // NOTE (Mathew): Revert back to meters because RenderSubTexture will convert from meters to pixels.
+            render_pos.x /= display->pixels_per_meter;
+            render_pos.y /= display->pixels_per_meter;
+
+            RenderSubTextureEx(display, &render_pos, &font->texture_sheet, sub_tex_x, sub_tex_y, font->char_width, font->char_height, colour);
+        }
+
+        char_num++;
+    }
+}
+
+void LoadTexture(char* file_path, Texture* dst_texture, SDL_PixelFormat* pixel_format) {
+    SDL_assert(!dst_texture->pixels);
+
+    SDL_Surface* loaded_texture = NULL;
+
+    loaded_texture = IMG_Load(file_path);
+    if(!loaded_texture) {
+        printf("Warning - could not load image '%s'.\n", file_path);
+        return;
+    }
+
+    SDL_Surface* corrected_texture = SDL_ConvertSurface(loaded_texture, pixel_format, 0);
+    if(!corrected_texture) {
+        printf("Warning - could not convert image. %s.\n", SDL_GetError());
+        return;
+    }
+
+    dst_texture->width = corrected_texture->w;
+    dst_texture->height = corrected_texture->h;
+    i32 tex_size = dst_texture->width * dst_texture->height;
+    dst_texture->pixels = malloc(tex_size * pixel_format->BytesPerPixel);
+    for(i32 i = 0; i < tex_size; i++) {
+        ((u32*)dst_texture->pixels)[i] = ((u32*)corrected_texture->pixels)[i];
+    }
+
+    SDL_FreeSurface(loaded_texture);
+    SDL_FreeSurface(corrected_texture);
+}
+
+void FreeTexture(Texture* texture) {
+    free(texture->pixels);
+    texture->pixels = 0;
+}
+
+void ReadEntireFile(char* path, File_Read_Result* read_result) {
+    SDL_assert(!read_result->data);
+    read_result->data = NULL;
+
+    SDL_RWops* file_handle = SDL_RWFromFile(path, "r");
+
+    if(file_handle) {
+        SDL_RWseek(file_handle, 0, RW_SEEK_END);
+        // NOTE (Mathew): +1 for EOF at the end of the file
+        read_result->size = SDL_RWtell(file_handle) + 1;
+        SDL_RWseek(file_handle, 0, RW_SEEK_SET);
+        read_result->data = malloc(read_result->size);
+
+        // NOTE (Mathew): Because we are reading the whole file a 0 means an error
+        i32 read_read_result = SDL_RWread(file_handle, read_result->data, read_result->size - 1, 1);
+
+        if(!read_read_result) { 
+            // TODO (Mathew): Log Error!
+            printf("Couldn't read the file: %s\n", path);
+        }
+
+        *((char*)read_result->data + read_result->size - 1) = EOF;
+        SDL_RWclose(file_handle);
+    }
+    else {
+        printf("Could Not Read The File: %s\n", path);
+    }
+}
+
+void WriteEntireFile(char* path, void* data, u64 size) {
+    SDL_RWops* file_handle = SDL_RWFromFile(path, "w");
+
+    if(file_handle) {
+        SDL_RWwrite(file_handle, data, size, 1);
+
+        SDL_RWclose(file_handle);
+
+        printf("File Written %s\n", path);
+     }
+     else {
+        // Log Error
+        printf("Couldn't write to file\n");
+     }
+}
+
+void FreeFileReadResult(File_Read_Result* file_read_result) {
+    free(file_read_result->data);
+    file_read_result->size = 0;
+}
+
+char char_set[36] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+void LoadFont(Font* font, SDL_PixelFormat* format) {
+    font->char_set_size = 36;
+    font->char_set = char_set;
+    font->char_width = 32;
+    font->char_height = 32;
+    font->tailed_char_offset = 16;
+    font->char_spacing = 0;
+
+    LoadTexture("../res/textures/font_texture.png", &font->texture_sheet, format);
+}
+
+void FreeFont(Font* font) {
+    font->char_set = 0;
+    FreeTexture(&font->texture_sheet);
+}
+
+#endif 
